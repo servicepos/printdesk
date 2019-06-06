d048d01c6f7a15e871b818c101d0ec27a03d7c44
Lasse Melbye <lmelbye@gmail.com>
2019-06-06 11:11:30 +0200

    building something

--- autoupdate.js
+++ autoupdate.js
@@ -1,28 +1,4 @@
-const log = require('electron-log');
-const electron = require('electron')
-const autoUpdater = electron.autoUpdater
-
-log.info('auto update loaded');
-
-
-autoUpdater.on('error', (ev, err) => {
-	log.info(err);
+require('update-electron-app')({
+	logger: log
 })
 
-autoUpdater.once('checking-for-update', (ev, err) => {
-	log.info('Checking for updates');
-})
-
-autoUpdater.once('update-available', (ev, err) => {
-	log.info('Update available. Downloading');
-})
-
-autoUpdater.once('update-not-available', (ev, err) => {
-	log.info('Update not available');
-})
-
-autoUpdater.once('update-downloaded', (ev, err) => {
-	log.info('Update downloaded');
-})
-
-autoUpdater.checkForUpdates()
