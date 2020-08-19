7f5897f2446a6996631222c1682b497e70c26468
Lasse Melbye <lmelbye@gmail.com>
2020-08-19 13:50:42 +0200

    Removed compatibility for old bamdesk, which broke current Printdesk bamdesk

--- bamdesk.js
+++ bamdesk.js
@@ -16,7 +16,7 @@ const { spawn } = require('child_process');
 const ps = require('ps-node');
 var commandExists = require('command-exists');
 
-let bamdeskProcess;
+let bamdeskProcess = null;
 let currentDevice;
 let askedForMono = false;
 let featureFlags;
@@ -29,11 +29,12 @@ async function keepAlive(device, ff) {
 	if (device) {
 		try {
 			const monoOK = await isMonoOK();
-			log.info(`Mono ok: ${monoOK}`);
+			log.info(`Mono status:`);
+			log.info(monoOK)
 		} catch (e) {
 			/* ask for mono once per instance (do not spam the user) */
-			log.error(`Mono failed: ${e}`);
-
+			log.error(`Mono failed:`);
+			log.error(e);
 			if (!askedForMono) {
 				dialog.showErrorBox(`Mono missing`, `You must install mono https://www.mono-project.com/download/stable/ to use the payment device driver`);
 				askedForMono = true;
@@ -42,17 +43,8 @@ async function keepAlive(device, ff) {
 		}
 	}
 
+	log.info({bamdeskProcess});
 	if (!bamdeskProcess) {
-		/* initiate */
-		try {
-			/* kill and uninstall any legacy bamdesk, if a device is select using the now method */
-			if (device) {
-				killLegacyBamdesk();
-				renameLegacyBamdesk();
-			}
-		} catch (e) {
-			/* no legacy bamdesk was running, just continue */
-		}
 		currentDevice = device;
 		await run();
 	} else { /* change device state */
@@ -72,21 +64,14 @@ async function keepAlive(device, ff) {
 /* start bamdesk if not running */
 async function run() {
 
+	log.info('Starting instance', currentDevice);
+
 	if (!currentDevice) {
 		log.info(`No device selected`);
 		bamdeskProcess = null;
 		return;
 	}
 
-	const running = await isBamdeskRunning();
-
-	if (running) {
-		log.info(`bamdesk already running`);
-		return false;
-	}
-
-
-	log.info('Starting instance', currentDevice);
 	let bamdeskExec;
 	if (featureFlags.bamdesktcp) {
 		bamdeskExec = path.join(__dirname, 'assets', 'BamdeskMintTCP.exe').replace('app.asar', 'app.asar.unpacked')
@@ -135,43 +120,6 @@ async function run() {
 	});
 }
 
-function killLegacyBamdesk() {
-	let cmd;
-	switch (os.platform()) {
-		case 'darwin':
-		case 'linux':
-			cmd = 'killall mono';
-			break;
-		case 'win32':
-			cmd = 'taskkill /im Bamdesk.exe';
-			break;
-		default:
-			throw new Exception('Platform not supported.');
-	}
-	log.info(`Kill any legacy bamdesk ${cmd}`);
-	const timeout = 1000;
-	cmdPromise(cmd, {}, {timeout}).then(e => log.info, log.error);
-}
-
-function isBamdeskRunning() {
-	let cmd;
-	switch (os.platform()) {
-		case 'darwin':
-		case 'linux':
-			cmd = 'ps';
-			break;
-		case 'win32':
-			cmd = 'tasklist';
-			break;
-		default:
-			throw new Exception('Platform not supported.');
-	}
-	return cmdPromise(cmd).then(out => {
-		log.info(`ps: ${out.stdout}`);
-		return out.stdout.indexOf('BamdeskMint.exe') > -1 || out.stdout.indexOf('Bamdesk.exe') > -1;
-	});
-}
-
 function isMonoOK() {
 	if (os.platform() == 'win32') {
 		return new Promise((resolve, reject) => {
@@ -186,37 +134,13 @@ function isMonoOK() {
 
 function monoEnv() {
 	const env = process.env;
-	const monoPath = `${env['PATH']}:/Library/Frameworks/Mono.framework/Versions/Current/Commands`;
+	const monoPath = `${env['PATH']}:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/Library/Frameworks/Mono.framework/Versions/Current/bin`;
 	return {
 		...env,
 		PATH : monoPath,
 	};
 }
 
-
-/** expires  */
-function renameLegacyBamdesk() {
-	log.info('rename legacy bamdesk');
-	try {
-		switch (os.platform()) {
-			case 'darwin':
-				const from = '/Applications/bamdesk';
-				const to = '/Applications/bamdesk_legacy';
-				log.info('Rename', {to, from});
-				fs.renameSync(from, to);
-				break;
-			case 'win32':
-				const cmd = `schtasks /CHANGE /tn "ServicePOS bamdesk" /DISABLE`;
-				log.info('disable cron:', cmd);
-				cmdPromise(cmd).then(e => log.info('Ok removed schtasks',e), log.error);
-				break;
-			default:
-				throw new Exception('Platform not supported.');
-		}
-	} catch (e) {
-		log.error('Error remiving legacy bamdesk', e);
-	}
-}
 
 module.exports = {
 	keepAlive,
